ea4fed468e493f7d4631fd365f29ec0370cab16f
h.kina <h.kina@dcom-net.jp>
2021-07-28 14:45:36 +0900

    別ブランチの新規作成

--- EmployeeManegementApp/EmployeeManagementWebUI/Controllers/SCRN0001/SCRN0001Controller.cs
+++ EmployeeManegementApp/EmployeeManagementWebUI/Controllers/SCRN0001/SCRN0001Controller.cs
@@ -7,10 +7,20 @@ using System.Threading.Tasks;
 namespace EmployeeManagementWebUI.Controllers.SCRN0001
 {
     /// <summary>
-    /// 
+    /// 社員管理システムメニュー　コントローラー
     /// </summary>
+    /// <remarks>
+    /// 社員管理システムメニュー　コントローラー
+    /// </remarks>
     public class SCRN0001Controller : Controller
     {
+        /// <summary>
+        /// 初期表示
+        /// </summary>
+        /// <remarks>
+        /// イベント名：社員管理システムメニュー画面初期表示
+        /// </remarks>
+        /// <returns>ActionResult</returns>
         [Route("")]
         [Route("/employee/menu")]
         [HttpGet]
