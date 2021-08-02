f7bdfbc4e9b8519bdc96a05102751a3a593f6276
kinahiroki <62067896+kinahiroki@users.noreply.github.com>
2021-08-03 08:51:10 +0900

    社員管理システム　更新
    社員照会・更新画面の新規作成
    画面の一部更新

--- EmployeeManegementApp/EmployeeManagementWebUI/Helper/Interface/IEV0001Helper.cs
+++ EmployeeManegementApp/EmployeeManagementWebUI/Helper/Interface/IEV0001Helper.cs
@@ -1,13 +1,22 @@
 ﻿using EmployeeManagementWebUI.ViewModel.SCRN0001;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace EmployeeManagementWebUI.Helper
 {
+    /// <summary>
+    /// メニュー画面ヘルパー　インターフェース
+    /// </summary>
+    /// <remarks>
+    /// メニュー画面ヘルパー　インターフェース
+    /// </remarks>
     public interface IEV0001Helper
     {
+        /// <summary>
+        /// メニュー画面　初期表示処理
+        /// </summary>
+        /// <remarks>
+        /// メニュー画面の初期表示時の処理を実施
+        /// </remarks>
+        /// <returns>表示情報</returns>
         SCRN0001ViewModelDTO Init();
     }
 }
--- EmployeeManegementApp/EmployeeManagementWebUI/Startup.cs
+++ EmployeeManegementApp/EmployeeManagementWebUI/Startup.cs
@@ -26,7 +26,7 @@ namespace EmployeeManagementWebUI
         {
             // DI‚ÅŽg‚¤ƒNƒ‰ƒX‚Ì’Ç‰Á
             // Helper
-            services.AddTransient<IEV0001Helper, EV0001Helper>();
+            services.AddSingleton<IEV0001Helper, EV0001Helper>();
             services.AddTransient<IEV0002Helper, EV0002Helper>();
 
             // Logic
