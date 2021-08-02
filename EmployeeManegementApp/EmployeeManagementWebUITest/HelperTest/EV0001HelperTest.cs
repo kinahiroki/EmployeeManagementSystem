5b98499f6089575166474267a96f80f9796cb1f7
h.kina <h.kina@dcom-net.jp>
2021-08-02 17:59:25 +0900

    社員管理システムメニュー画面　Helper NUnit新規作成

---
+++ EmployeeManegementApp/EmployeeManagementWebUITest/HelperTest/EV0001HelperTest.cs
@@ -0,0 +1,40 @@
+﻿using EmployeeManagementWebUI.Helper;
+using NUnit.Framework;
+
+namespace EmployeeManagementWebUITest.HelperTest
+{
+    /// <summary>
+    /// メニュー画面ヘルパーテストクラス
+    /// </summary>
+    /// <remarks>
+    /// メニュー画面ヘルパーテストクラス
+    /// </remarks>
+    [TestFixture]
+    public class EV0001HelperTest
+    {
+        /// <summary>
+        /// 初期表示処理　正常テスト
+        /// </summary>
+        /// <remarks>
+        /// 初期表示時の正常時テストを行う。
+        /// 実施結果が以下の通りになることを検証する。
+        /// ①ViewModelDTOの各項目が初期値で格納されていること
+        /// 　社員ID                ：空文字
+        /// 　エラーメッセージリスト：0件
+        /// </remarks>
+        [Test]
+        public void InitNormalTest()
+        {
+            // Arrange
+            // なし
+
+            // Act
+            var target = new EV0001Helper();
+            var actualResult = target.Init();
+
+            // Assert
+            Assert.AreEqual(string.Empty, actualResult.EmployeeID);
+            CollectionAssert.IsEmpty(actualResult.ErrorMessageList);
+        }
+    }
+}
