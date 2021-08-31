c01b5bf13294b913ed49310f53091ba9955d531a
h.kina <h.kina@dcom-net.jp>
2021-08-31 13:28:00 +0900

    DBアクセスクラスのサンプル、Repositoryクラスの新規追加

---
+++ EmployeeManegementApp/EmployeeManagementWebUI/DataAccess/SampleDataAccess.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementWebUI.DataAccess
+{
+    /// <summary>
+    /// Sample DataAccess
+    /// </summary>
+    public class SampleDataAccess
+    {
+        public void SampleSelect()
+        {
+            using var repository = new EmployeeSystemRepository();
+
+            // ①DB接続の開始
+            repository.Open();
+
+            // ②SQLの生成
+
+            // ③SQLのパラメータ設定
+
+            // ④SQLの実行
+
+            // ⑤SQL結果の取得
+
+            // ⑥DB接続の終了
+            repository.Clone();
+        }
+    }
+}
