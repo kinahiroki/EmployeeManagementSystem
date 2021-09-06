56a22a154b217caaf1707179135d618b3608df31
h.kina <h.kina@dcom-net.jp>
2021-09-06 16:41:38 +0900

    SQL Selectのサンプル新規追加

---
+++ EmployeeManegementApp/EmployeeManagementWebUI/DataAccess/DataAccessSample.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementWebUI.DataAccess
+{
+    /// <summary>
+    /// DB接続のサンプル
+    /// </summary>
+    public class DataAccessSample
+    {
+        /// <summary>
+        /// SQLパラメータありのコードサンプル
+        /// </summary>
+        public void SelectSampleWithParam()
+        {
+            using var repository = new EmployeeSystemRepository();
+            repository.Open();
+
+            // ①引数ありのSQLを実行する場合
+            var selectQueryWithParam = "SELECT * FROM table_name WHERE target_column_name = @variableName";
+
+            // ②引数のパラメータ名と値をDictionaryに格納する。
+            var parametorNameAndValueDic = new Dictionary<string, object>()
+            {
+                // { SQLに指定した変数名, 変数に入れたい値 }
+                { "@variableName",  string.Empty }
+            };
+
+            // ③SQLの実行
+            var selectResult = repository.ExcuteQuery(selectQueryWithParam, parametorNameAndValueDic);
+            while (selectResult.Read())
+            {
+            }
+            selectResult.Close();
+
+            repository.Close();
+        }
+
+        /// <summary>
+        /// SQLパラメータなしのコードサンプル
+        /// </summary>
+        public void SelectSampleNoParam()
+        {
+            using var repository = new EmployeeSystemRepository();
+            repository.Open();
+
+            // ①引数ありのSQLを実行する場合
+            var selectQueryWithParam = "SELECT * FROM table_name WHERE target_column_name";
+
+            // ②SQLの実行
+            var selectResult = repository.ExcuteQuery(selectQueryWithParam);
+            while (selectResult.Read())
+            {
+            }
+            selectResult.Close();
+
+            repository.Close();
+        }
+    }
+}
